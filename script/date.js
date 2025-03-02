933b096329559c0cda8a023b14bf2d27d1c4d8ae
imon <imon.eee@gmail.com>
2025-03-02 12:54:24 +0600

    date updated

--- script/date.js
+++ script/date.js
@@ -1,3 +1,4 @@
-const d = new Date();
-
-console.log(d);
+const date = new Date().toDateString().split(" ");
+document.getElementById(
+  "date"
+).innerHTML = `${date[0]}<br> <span class="font-semibold">${date[1]} ${date[2]} ${date[3]}</span>`;
